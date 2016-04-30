21f524640930f3161b786e24d4a35d48478079d8
NikhilG100 <buddhibal100@gmail.com>
2016-04-30 14:29:51 -0700

    Delete NotifySearchResults.java

--- NotifySearchResults.java
+++
@@ -1,9 +0,0 @@
-package com.example.nikhil.wikipediasearch;
-
-/**
- * Created by Nikhil on 4/30/2016.
- */
-public interface NotifySearchResults {
-    void OnSearchCompleted();
-    void OnSearchInProgress();
-}
