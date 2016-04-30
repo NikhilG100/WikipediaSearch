c32779c73f763b03719dbdfee481268c1ca5df94
NikhilG100 <buddhibal100@gmail.com>
2016-04-30 14:29:11 -0700

    Added files via upload

---
+++ app/src/main/java/com/example/nikhil/wikipediasearch/NotifySearchResults.java
@@ -0,0 +1,9 @@
+package com.example.nikhil.wikipediasearch;
+
+/**
+ * Created by Nikhil on 4/30/2016.
+ */
+public interface NotifySearchResults {
+    void OnSearchCompleted();
+    void OnSearchInProgress();
+}
